7a023dba7632bde22574dd0a135f237669a1ca03
ahannula7324 <ahannula7324@sdsu.edu>
2026-02-24 20:05:24 -0800

    Added all the "#include <>" to the program as well as the static int MAX_SPACES.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,11 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+static const int MAX_SPACES = 40;
 
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
