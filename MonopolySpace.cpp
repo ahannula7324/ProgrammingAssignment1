db5e73532a3e6d0eb0d5d5aac7d4da2326ead5ed
ahannula7324 <ahannula7324@sdsu.edu>
2026-03-04 18:20:49 -0800

    Class additons and basic structure set.

---
+++ MonopolySpace.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+static const int MAX_SPACES = 40;
+
+
+class MonopolySpace {
+    public:
+    string propertyName;
+    string propertyColor;
+    int value;
+    int rent;
+
+};
